7c084c8e02bd9b3b28b6bb30266c4ad3a60ff3d2
Abdoul111 <93938245+Abdoul111@users.noreply.github.com>
2023-01-22 01:40:47 -0500

    overview page is partially done

--- lib/page-1/Overview1.dart
+++ lib/page-1/Overview1.dart
@@ -11,6 +11,16 @@ class Overview1 extends StatefulWidget {
 }
 
 class OverviewState extends State<Overview1> {
+
+  List<UncategorizedItem> items = [
+    UncategorizedItem("Tim Hortons", "12 Jan", 15),
+    UncategorizedItem("McDonalds", "12 Jan", 15),
+    UncategorizedItem("Starbucks", "12 Jan", 15),
+    UncategorizedItem("Starbucks", "12 Jan", 15),
+    UncategorizedItem("Starbucks", "12 Jan", 15),
+
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -72,162 +82,98 @@ class OverviewState extends State<Overview1> {
                 heightFactor: 4,
               ),
 
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: SingleChildScrollView(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage('Path to your image'),
-                      ),
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(30.0), topRight: Radius.circular(30.0)),
-                      color: Color(0xff2F2F2F),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.5),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: const Offset(0, 3), // changes position of shadow
-                        ),
-                      ],
+              Positioned(
+                child: Container(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10.0, bottom: 20.0, top: 30.0),
+                  height: 300,
+                  decoration: BoxDecoration(
+                    image: const DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage('Path to your image'),
                     ),
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(30.0), topRight: Radius.circular(30.0)),
+                    color: Color(0xff2F2F2F),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: const Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
 
+                  child: SingleChildScrollView(
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.all(15),
-                          child: Row(
-                            children: [
-                              const Padding(
-                                padding: EdgeInsets.only(left: 5, right: 5, bottom: 5),
-                                child: Text(
-                                  'Date',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                              const Padding(
-                                padding: EdgeInsets.only(left: 5, right: 5, bottom: 5),
-                                child: Text(
-                                  'Category',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                              const Padding(
-                                padding: EdgeInsets.only(left: 5, right: 5, bottom: 5),
-                                child: Text(
-                                  'Amount',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
+                      crossAxisAlignment: CrossAxisAlignment.center,
 
-                        Container(
-                          margin: const EdgeInsets.all(15),
-                          child: Row(
-                            children: [
-                              const Padding(
-                                padding: EdgeInsets.only(left: 5, right: 5, bottom: 5),
-                                child: Text(
-                                  'Date',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                              const Padding(
-                                padding: EdgeInsets.only(left: 5, right: 5, bottom: 5),
-                                child: Text(
-                                  'Category',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                              const Padding(
-                                padding: EdgeInsets.only(left: 5, right: 5, bottom: 5),
-                                child: Text(
-                                  'Amount',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-
-                        Container(
-                          margin: const EdgeInsets.all(15),
-                          child: Row(
-                            children: [
-                              const Padding(
-                                padding: EdgeInsets.only(left: 5, right: 5, bottom: 5),
-                                child: Text(
-                                  'Date',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                              const Padding(
-                                padding: EdgeInsets.only(left: 5, right: 5, bottom: 5),
-                                child: Text(
-                                  'Category',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                              const Padding(
-                                padding: EdgeInsets.only(left: 5, right: 5, bottom: 5),
-                                child: Text(
-                                  'Amount',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
+                      children: uncategorizedListBuilder()
                     ),
                   ),
                 ),
               ),
-
             ],
           ),
         ],
       ),
     );
   }
+
+
+  List<Widget> uncategorizedListBuilder() {
+    List<Widget> uncategorizedList = [];
+    for (int i = 0; i < items.length; i++) {
+      uncategorizedList.add(
+        Container(
+          margin: const EdgeInsets.all(15),
+          child: Row(
+            children: [
+               Padding(
+                padding: EdgeInsets.only(left: 5, right: 5, bottom: 5),
+                child: Text(
+                  items[i].name,
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 5, right: 5, bottom: 5),
+                child: Text(
+                  items[i].date,
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 5, right: 5, bottom: 5),
+                child: Text(
+                  items[i].price.toString(),
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+    return uncategorizedList;
+  }
+}
+
+class UncategorizedItem {
+  String name;
+  String date;
+  double price;
+
+  UncategorizedItem(this.name, this.date, this.price);
 }
