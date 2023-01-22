6cad35a09bab53fab7c4722f18a48e87e2d59cc4
philippenikolov <philippe.nikolov@outlook.com>
2023-01-22 09:56:14 -0500

    start goals page

---
+++ lib/page-1/EditCategory.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class EditCategory extends StatefulWidget {
+  const EditCategory({super.key});
+
+  @override
+  EditCategoryState createState() => EditCategoryState();
+}
+
+class EditCategoryState extends State<EditCategory> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color(0xff252525),
+      ),
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          color: Color(0xff252525),
+        ),
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Padding(
+            padding: EdgeInsets.only(left: 10, right: 10, top: 20),
+            child: Container(
+                height: 220,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  color: Color(0xff2f2f2f),
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 32,
+                    ),
+                    Row(
+                      children: [
+                        Spacer(),
+                        Text('Name', style:TextStyle(color: Color(0xffffffff,), fontSize: 18)),
+                        Spacer(),
+                        SizedBox(
+                          width: 200,
+                          child: TextField(
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Color(0xff353535),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: BorderSide(color: Color(0xff353535), width: 0.5),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Spacer(),
+                      ],
+                    ),
+
+                    SizedBox(
+                      height: 26,
+                    ),
+                    Row(
+                      children: [
+                        Spacer(),
+                        Text('Percentage', style:TextStyle(color: Color(0xffffffff,), fontSize: 18)),
+                        Spacer(),
+                        SizedBox(
+                          width: 200,
+                          child: TextField(
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Color(0xff353535),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: BorderSide(color: Color(0xff353535), width: 0.5),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Spacer(),
+                      ],
+                    ),
+                  ],
+                )),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/page-1/Goals.dart
+++ lib/page-1/Goals.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:myapp/database/CategoriesModel.dart';
+import 'package:myapp/page-1/EditCategory.dart';
 import 'package:myapp/page-1/TransactionPage.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
@@ -10,6 +11,7 @@ class Goals extends StatefulWidget {
   @override
   GoalsState createState() => GoalsState();
 }
+
 class ChartData {
   ChartData(this.x, this.y /*,  this.color */);
 
@@ -17,156 +19,188 @@ class ChartData {
   final double? y;
 // final Color color;
 }
+
 class GoalsState extends State<Goals> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: const EdgeInsets.only(top:20,bottom: 20,right: 10,left: 10),
-
+        padding:
+            const EdgeInsets.only(top: 20, bottom: 20, right: 10, left: 10),
         width: double.infinity,
-        decoration: const BoxDecoration (
+        decoration: const BoxDecoration(
           color: Color(0xff252525),
         ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(left:10, bottom:10, top: 20),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 10, bottom: 10, top: 20),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
 //              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(flex:5,child: Text("Budget", style: TextStyle(color: Color(0xffffffff), fontSize: 26,),textAlign: TextAlign.left,)),
-                Expanded(
-                  child: IconButton(
-                    icon: const Icon(Icons.add, size: 40,),
-                    color: Colors.white,
-                    onPressed: () {
-                      setState(() {
-                        //TODO add new category page
-                        CategoriesModel.dummyData();
-                      });
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Center(
-            child: SfCircularChart(
-              series: <CircularSeries<ChartData, String>>[
-                DoughnutSeries<ChartData, String>(
-                    dataSource: getChartData(),
-                    xValueMapper: (ChartData data, _) => data.x,
-                    yValueMapper: (ChartData data, _) => data.y,
-                    dataLabelMapper: (ChartData data, _) => data.x,
-                    radius: '60%',
-                    dataLabelSettings: const DataLabelSettings(
-                        isVisible: true,
-                        textStyle: TextStyle(color: Colors.white),
-                        // Avoid labels intersection
-                        labelIntersectAction: LabelIntersectAction.shift,
-                        labelPosition: ChartDataLabelPosition.outside,
-                        connectorLineSettings: ConnectorLineSettings(
-                            type: ConnectorType.curve, length: '25%')
+                children: [
+                  Expanded(
+                      flex: 5,
+                      child: Text(
+                        "Budget",
+                        style: TextStyle(
+                          color: Color(0xffffffff),
+                          fontSize: 26,
+                        ),
+                        textAlign: TextAlign.left,
+                      )),
+                  Expanded(
+                    child: IconButton(
+                      icon: const Icon(
+                        Icons.add,
+                        size: 40,
+                      ),
+                      color: Colors.white,
+                      onPressed: () {
+                        setState(() {
+                          //TODO add new category page
+                          CategoriesModel.dummyData();
+                        });
+                      },
                     ),
-                    explode: true,
-                    explodeOffset: "28",
-                    explodeGesture: ActivationMode.singleTap
-                )
-              ] ,
-            ),
-          ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.only(left: 5,right: 5),
-              //width: double.infinity,
-              decoration: const BoxDecoration (
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: Color(0xff2f2f2f),
+                  ),
+                ],
               ),
-              child: LayoutBuilder(
-                builder: (context, constraint) {
-                  return SingleChildScrollView(
-                    child: Column(
-                      children: List.generate(CategoriesModel.list.length, (index) => Padding(
-                        padding: const EdgeInsets.only(top:10,bottom: 10),
-                        child:
-                        Slidable(
-                          key: const ValueKey(0),
+            ),
+            Center(
+              child: SfCircularChart(
+                series: <CircularSeries<ChartData, String>>[
+                  DoughnutSeries<ChartData, String>(
+                      dataSource: getChartData(),
+                      xValueMapper: (ChartData data, _) => data.x,
+                      yValueMapper: (ChartData data, _) => data.y,
+                      dataLabelMapper: (ChartData data, _) => data.x,
+                      radius: '60%',
+                      dataLabelSettings: const DataLabelSettings(
+                          isVisible: true,
+                          textStyle: TextStyle(color: Colors.white),
+                          // Avoid labels intersection
+                          labelIntersectAction: LabelIntersectAction.shift,
+                          labelPosition: ChartDataLabelPosition.outside,
+                          connectorLineSettings: ConnectorLineSettings(
+                              type: ConnectorType.curve, length: '25%')),
+                      explode: true,
+                      explodeOffset: "28",
+                      explodeGesture: ActivationMode.singleTap)
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.only(left: 5, right: 5),
+                //width: double.infinity,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  color: Color(0xff2f2f2f),
+                ),
+                child: LayoutBuilder(
+                  builder: (context, constraint) {
+                    return SingleChildScrollView(
+                      child: Column(
+                        children: List.generate(
+                          CategoriesModel.list.length,
+                          (index) => Padding(
+                            padding: const EdgeInsets.only(top: 10, bottom: 10),
+                            child: Slidable(
+                              key: const ValueKey(0),
+                              endActionPane: ActionPane(
+                                // A motion is a widget used to control how the pane animates.
+                                motion: const ScrollMotion(),
 
-                          endActionPane: ActionPane(
-                            // A motion is a widget used to control how the pane animates.
-                            motion: const ScrollMotion(),
+                                // A pane can dismiss the Slidable.
+                                dismissible: null,
 
-                            // A pane can dismiss the Slidable.
-                            dismissible: null,
-
-
-                            // All actions are defined in the children parameter.
-                            children: [
-                              // A SlidableAction can have an icon and/or a label.
-                              SlidableAction(
-                                onPressed: (BuildContext context) {
-                                  // Navigator.push(
-                                  //   context,
-                                  //   MaterialPageRoute(builder: (context) => const EditTransaction()),
-                                  // );
-                                },
-                                backgroundColor: Color(0xFF21B7CA),
-                                foregroundColor: Colors.white,
-                                icon: Icons.edit,
+                                // All actions are defined in the children parameter.
+                                children: [
+                                  // A SlidableAction can have an icon and/or a label.
+                                  SlidableAction(
+                                    onPressed: (BuildContext context) {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const EditCategory()),
+                                      );
+                                    },
+                                    backgroundColor: Color(0xFF21B7CA),
+                                    foregroundColor: Colors.white,
+                                    icon: Icons.edit,
+                                  ),
+                                  SlidableAction(
+                                    onPressed: (BuildContext context) {
+                                      setState(() {
+                                        if (CategoriesModel.list.length == 1) {
+                                          CategoriesModel(0, 'No Category', 1);
+                                        }
+                                        CategoriesModel.delete(index);
+                                      });
+                                    },
+                                    backgroundColor: Color(0xFFFE4A49),
+                                    foregroundColor: Colors.white,
+                                    icon: Icons.delete,
+                                  ),
+                                ],
                               ),
-                              SlidableAction(
-
-                                onPressed: (BuildContext context) {
-                                  setState(() {
-                                    if(CategoriesModel.list.length==1) {
-                                      CategoriesModel( 0, 'No Category',1);
-                                    }
-                                    CategoriesModel.delete(index);
-                                  });
-                                },
-
-                                backgroundColor: Color(0xFFFE4A49),
-                                foregroundColor: Colors.white,
-                                icon: Icons.delete,
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 10, bottom: 10),
+                                child: Row(
+                                  children: [
+                                    const Spacer(),
+                                    Text(
+                                        CategoriesModel.list
+                                            .elementAt(index)
+                                            .name,
+                                        style: TextStyle(
+                                            color: Color(
+                                              0xffffffff,
+                                            ),
+                                            fontSize: 18)),
+                                    const Spacer(
+                                      flex: 4,
+                                    ),
+                                    Text(
+                                        (CategoriesModel.list
+                                                        .elementAt(index)
+                                                        .percentage *
+                                                    100)
+                                                .toInt()
+                                                .toString() +
+                                            "%",
+                                        style: TextStyle(
+                                            color: Color(0xffffffff),
+                                            fontSize: 18)),
+                                    const Spacer(),
+                                  ],
+                                ),
                               ),
-                            ],
-                          ), child:
-                        Padding(
-                          padding: const EdgeInsets.only(top:10,bottom: 10),
-                          child: Row(
-                            children: [
-                              const Spacer(),
-                              Text(CategoriesModel.list.elementAt(index).name, style:TextStyle(color: Color(0xffffffff,), fontSize: 18)),
-                              const Spacer(flex: 4,),
-                              Text((CategoriesModel.list.elementAt(index).percentage*100).toInt().toString()+"%", style:TextStyle(color: Color(0xffffffff), fontSize: 18)),
-                              const Spacer(),
-                            ],
+                            ),
                           ),
                         ),
-                        ),
                       ),
-                      ),
-                    ),
-                  );},
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-        ],
-      ),
+          ],
+        ),
       ),
     );
   }
 
-  List<ChartData> getChartData(){
+  List<ChartData> getChartData() {
     List<ChartData> chartData = <ChartData>[];
     for (var i = 0; i < CategoriesModel.list.length; i++) {
-      chartData.add(ChartData(CategoriesModel.list[i].name, CategoriesModel.list[i].percentage*100));
+      chartData.add(ChartData(CategoriesModel.list[i].name,
+          CategoriesModel.list[i].percentage * 100));
     }
     return chartData;
   }
-
 }
